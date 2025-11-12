d9f764bda782dff841d1ba008088a72e4d65426d
imrajputkartik <kartikchauhan647@gmail.com>
2025-11-12 12:59:34 +0530

    update

--- src/main/java/com/bhanu/spring/Main.java
+++ src/main/java/com/bhanu/spring/Main.java
@@ -13,8 +13,7 @@ public class Main {
 		Employee bean= ioc.getBean("e1", Employee.class);
 		System.out.println(bean);
 		
-		Employee bean2= ioc.getBean("e2", Employee.class);
-		System.out.println(bean2);
+		
 	}
 
 }
