1f3dc95d373159c19fbbca6b6d1079f666fe2d15
imrajputkartik <kartikchauhan647@gmail.com>
2025-11-13 10:16:53 +0530

    update

--- src/main/java/com/bhanu/spring/model/Employee.java
+++ src/main/java/com/bhanu/spring/model/Employee.java
@@ -1,22 +1,33 @@
+
 package com.bhanu.spring.model;
 
 public class Employee {
 
 	private String name, gender;
 	private int id;
+	private Address address;
 	
 	
 	public Employee() {
-		super();
-		// TODO Auto-generated constructor stub
+		System.out.println("Employee.employee()");
 	}
 
-
-	public Employee(String name, String gender, int id) {
-		super();
+	public Employee(String name, String gender, int id, Address address) {
+		
 		this.name = name;
 		this.gender = gender;
 		this.id = id;
+		this.address = address;
+		System.out.println("Employee.employee(5)");
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+		System.out.println("Employee.setAddress()");
 	}
 
 
@@ -27,7 +38,7 @@ public class Employee {
 
 	public void setName(String name) {
 		this.name = name;
-		System.out.println("Employye.name()");
+		System.out.println("Employye.setName()");
 	}
 
 
@@ -38,7 +49,7 @@ public class Employee {
 
 	public void setGender(String gender) {
 		this.gender = gender;
-		System.out.println("Employee.gender()");
+		System.out.println("Employee.setGender()");
 	}
 
 
@@ -49,15 +60,13 @@ public class Employee {
 
 	public void setId(int id) {
 		this.id = id;
-		System.out.println("Employee.id()");
+		System.out.println("Employee.setId()");
 	}
 
 
 	@Override
 	public String toString() {
-		return "Employee [name=" + name + ", gender=" + gender + ", id=" + id + "]";
+		return "Employee [name=" + name + ", gender=" + gender + ", id=" + id + ", address=" + address + "]";
 	}
-	
-	
 	
 }
